f8e384f12daba9e8c940c2fe58bfb4938e4c39bc
mogr20 <markjordanrandall@gmail.com>
2025-02-25 14:29:42 +0000

    ADD: database models for blog posts

--- codestar/settings.py
+++ codestar/settings.py
@@ -95,6 +95,12 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
+# Trusted request origins
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com",
+]
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
