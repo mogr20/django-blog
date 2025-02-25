7b9d6be7934cc5d3940a220a1fe11972c4e17296
mogr20 <markjordanrandall@gmail.com>
2025-02-25 10:52:37 +0000

    UPDATE: reverting to debug mode for local development, now that the webhost has deployed

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uwpe2ez(u%)jfgrts7bob%hzqus)5rk=n65pk%c@t-&kab^p%1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
