cd4ae9d365762b90a8dad659eb48c72649878e10
mogr20 <markjordanrandall@gmail.com>
2025-02-25 14:19:34 +0000

    UPDATE: moved secret key to env.py and config vars on Heroku, and regenerated a new key

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uwpe2ez(u%)jfgrts7bob%hzqus)5rk=n65pk%c@t-&kab^p%1'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
