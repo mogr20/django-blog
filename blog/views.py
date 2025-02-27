5e0864d3dc124ae57884f81ff5f42f494c319465
mogr20 <markjordanrandall@gmail.com>
2025-02-27 10:10:11 +0000

    ADD: html templates and styling for blog posts and site overall

--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,6 @@ from .models import Post
 class PostList(generic.ListView):
     queryset = Post.objects.all().order_by("-created_on")
     # queryset = Post.objects.filter(status=1).order_by("-created_on")
-    template_name = "post_list.html"
+    # template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
