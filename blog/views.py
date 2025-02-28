cf55a4338d00893679090c164dc0c443903c7143
mogr20 <markjordanrandall@gmail.com>
2025-02-28 13:04:49 +0000

    ADD: about page, had to use source code as could not find error in my solution

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
-
 # Create your views here.
+
+
 class PostList(generic.ListView):
-    queryset = Post.objects.all().order_by("-created_on")
-    # queryset = Post.objects.filter(status=1).order_by("-created_on")
-    # template_name = "post_list.html"
+    queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
 
@@ -25,12 +24,6 @@ def post_detail(request, slug):
 
     :template:`blog/post_detail.html`
     """
-
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-
-    return render(
-        request,
-        "blog/post_detail.html",
-        {"post": post},
-    )
+    return render(request, "blog/post_detail.html", {"post": post},)
