131c121b88614e12f192825b4f62175d11c483ad
mogr20 <markjordanrandall@gmail.com>
2025-03-03 11:56:25 +0000

    UPDATE: collaboration now posts to database

--- about/views.py
+++ about/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
 # Create your views here.
@@ -9,6 +10,16 @@ def about_me(request):
     Renders the About page
     """
     about = About.objects.all().order_by('-updated_on').first()
+
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                "Collaboration request received! I endeavor to respond within 2 working days."
+            )
+    
     collaborate_form = CollaborateForm()
 
     return render(
